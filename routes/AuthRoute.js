6008aacceeee168304d18fc41fd59c74a0ba3572
SantoshKcy <kcsantosh2003@gmail.com>
2024-12-23 23:37:07 +0545

    Implemented user registration route

---
+++ routes/AuthRoute.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const router = express.Router();
+const { login, register } = require("../controller/AuthController");
+
+
+
+
+router.post("/register", register);
+
+
+
+module.exports = router;
+

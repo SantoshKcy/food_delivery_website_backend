d2652914f8552aacb23aeefa2c754a99049d44c5
SantoshKcy <kcsantosh2003@gmail.com>
2025-01-02 19:50:24 +0545

    review model added

---
+++ model/Review.js
@@ -0,0 +1,30 @@
+
+const mongoose = require("mongoose")
+const reviewSchema = new mongoose.Schema({
+    customerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "customers"
+    },
+    restaurantId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "restaurants"
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    comment: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+
+
+
+})
+const Review = mongoose.model("reviews", reviewSchema);
+
+module.exports = Review;
